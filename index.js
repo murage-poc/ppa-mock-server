8bd27514b0ea30965292c3fe203afc6185cf2ae4
murage <murage@labskenya.com>
2022-03-02 13:22:54 +0300

    fix:

--- index.js
+++ index.js
@@ -2,45 +2,49 @@ const path = require('path');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
-const router = jsonServer.router(path.join(__dirname, 'storage/db.json'),
-  { foreignKeySuffix: '_id' });
+
+const router = jsonServer.router(dbPath(),
+    {foreignKeySuffix: '_id', watch: dbPath()});
 
 const port = process.env.PORT ? process.env.PORT : 3000;
 
 router.render = (req, res) => {
-  if (Array.isArray(res.locals.data)) {
-    res.jsonp({
-      data: res.locals.data
-    });
-  } else {
-    res.jsonp({ ...res.locals.data })
-  }
+    if (Array.isArray(res.locals.data)) {
+        res.jsonp({
+            data: res.locals.data
+        });
+    } else {
+        res.jsonp({...res.locals.data})
+    }
 }
 
 //middlewares
 server.use(jsonServer.rewriter({
-  '/api/v1/products/items?*':'/products?$1',
-   '/api/v1/*': '/$1', 
-  }));
+    '/api/v1/products/items?*': '/products?$1',
+    '/api/v1/*': '/$1',
+}));
 server.use(jsonServer.bodyParser);
 
 server.use((req, res, next) => {
-  if (req.query && req.query.search) {
-    req.query['q'] = req.query.search;
-    req.query['_limit'] = 10;
-  }
+    if (req.query && req.query.search) {
+        req.query['q'] = req.query.search;
+        req.query['_limit'] = 10;
+    }
 
-  //embed
-  req.query['_embed']="created_by";
+    if (req.method === 'POST') {
+        req.body.created_at = Date.now()
+    }
 
-  if (req.method === 'POST') {
-    req.body.created_at = Date.now()
-  }
-
-  next();
+    next();
 });
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port, () => { console.log('JSON Server is running') })
+function dbPath() {
+    return path.join(__dirname, 'storage/db.json');
+}
+
+server.listen(port, () => {
+    console.log('JSON Server is running')
+});
